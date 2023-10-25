da63328e854683fa8e719f36ceaefdf13d493686
my <3177830400@qq.com>
2023-10-25 21:51:54 +0800

    项目结构目录

---
+++ webapi/Program.cs
@@ -0,0 +1,25 @@
+using Webapi;
+
+var builder = WebApplication.CreateBuilder(args); 
+builder.Services.AddControllers(); 
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+builder.Services.AddCoreService();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.AddCoreApp();
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
---
+++ webapi/Startup.cs
@@ -0,0 +1,15 @@
+﻿namespace Webapi
+{
+    public static class Startup
+    {
+        public static void AddCoreApp(this WebApplication app)
+        {
+
+        }
+        public static void AddCoreService(this IServiceCollection service)
+        {
+
+        }
+      
+    }
+}
