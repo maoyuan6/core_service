10462f2f0b77d9582dc58c1af3a123b98516061c
maoyuan <3177830400@qq.com>
2024-03-27 17:07:31 +0800

    新增log 实体

---
+++ Respository/Entities/Log/SystemLog.cs
@@ -0,0 +1,36 @@
+﻿using FreeSql.DataAnnotations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository.Entities.Log
+{
+    [Index("uk_id", "id", true)]
+    [Table(Name = "system_log")]
+    public class SystemLog : IEntityBase
+    {
+        /// <summary>
+        /// 
+        /// </summary>
+        [Column(IsIdentity = true, Name = "id")]
+        public int Id { get; set; }
+        /// <summary>
+        /// 日志类型
+        /// </summary>
+        [Column(IsNullable = false, Name = "type", StringLength = 128)]
+        public string Type { get; set; }
+        /// <summary>
+        /// 日志信息
+        /// </summary>
+        [Column(IsNullable = false, Name = "log_info", DbType = "LONGTEXT")]
+        public string LogInfo { get; set; }
+
+        /// <summary>
+        /// 日志记录时间
+        /// </summary>
+        [Column(IsNullable = false, Name = "log_time")]
+        public DateTime LogTime { get; set; }
+    }
+}
