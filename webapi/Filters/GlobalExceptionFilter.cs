66e1d452d635493631f85e5fb55bf25dfa87b5ad
maoyuan <3177830400@qq.com>
2024-03-27 16:41:08 +0800

    新增过滤器

---
+++ webapi/Filters/GlobalExceptionFilter.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FreeSql;
+using Infrastructure.Model;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Microsoft.IdentityModel.Logging;
+using Newtonsoft.Json;
+using Repository.Entities.Log;
+using Webapi.Controllers.Base;
+
+namespace Infrastructure.Filters
+{
+    public class GlobalExceptionFilter : HttpGlobalExceptionLogFilter
+    { 
+        public GlobalExceptionFilter(ILogger<HttpGlobalExceptionLogFilter> logger, IFreeSql freeSql) : base(logger, freeSql)
+        {
+        }
+        public Task OnExceptionAsync(ExceptionContext context)
+        {
+            OnException(context);
+            return Task.CompletedTask;
+        }
+
+        public void OnException(ExceptionContext context)
+        {
+            base.OnException(context);
+            if (context.Exception is BusinessException customException)
+            {
+                var json = new JsonResult(
+                    new
+                    {
+                        Success = false,
+                        Code = customException.HResult,
+                        customException.Message
+                    });
+                context.Result = json;
+            }
+            else
+            {
+                var json = new JsonResult(
+                    new
+                    {
+                        Success = false,
+                        Code = 500,
+                        context.Exception.Message
+                    });
+                context.Result = json;
+            }
+            context.ExceptionHandled = true;
+        }
+    }
+
+    public class HttpGlobalExceptionLogFilter : IExceptionFilter
+    {
+        private readonly ILogger<HttpGlobalExceptionLogFilter> logger;
+
+        private readonly IFreeSql _freeSql;
+        private readonly IBaseRepository<SystemLog> _logRepository;
+        public HttpGlobalExceptionLogFilter(ILogger<HttpGlobalExceptionLogFilter> logger, IFreeSql freeSql)
+        {
+            this.logger = logger;
+            _freeSql = freeSql;
+            _logRepository = _freeSql.GetRepository<SystemLog>();
+        }
+        public virtual void OnException(ExceptionContext context)
+        {  
+            if (context.Exception is not BusinessException)
+            {
+                //不是业务异常就记日志
+                _ = _logRepository.InsertAsync(new SystemLog()
+                {
+                    Type = "Error",
+                    LogInfo = context.Exception.Message + context.Exception.StackTrace,
+                    LogTime = DateTime.Now
+                });
+            } 
+            var json = new ResponseResult<string>()
+            {
+                Code = 500,
+                Message = context.Exception.Message
+            };
+            context.Result = new JsonResult(json);
+            context.ExceptionHandled = true;
+        }
+    }
+}
---
+++ webapi/Filters/TokenFilter .cs
@@ -0,0 +1,50 @@
+﻿using Infrastructure.JWT.Check;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Controllers;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Respository.Global;
+
+namespace Webapi.Filters
+{
+    public class TokenFilter : IAsyncActionFilter, IFilterMetadata
+    {
+        public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
+        {
+            bool isAnonymous = false;
+            //忽略认证的接口
+            if (context.ActionDescriptor is ControllerActionDescriptor descriptor)
+            {
+                if (descriptor != null)
+                {
+                    var actionAttributes = descriptor.MethodInfo.GetCustomAttributes(inherit: true);
+                    isAnonymous = actionAttributes.Any(a => a is AllowAnonymousAttribute);
+                }
+            }
+
+            if (!isAnonymous)
+            {
+                var request = context.HttpContext.Request;
+                var token = request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+                if (string.IsNullOrEmpty(token))
+                {
+                    context.Result = new StatusCodeResult(401); // 未授权状态码
+                    return;
+                }
+
+                if (!IsValidToken(token))
+                {
+                    context.Result = new StatusCodeResult(401); // 未授权状态码
+                    return;
+                }
+            }
+            await next();
+        }
+        private bool IsValidToken(string token)
+        {
+            IAuthenticationService? authenticationService = new Infrastructure.JWT.Check.AuthenticationService();
+            authenticationService.IsAuthenticated(out var isValid, token);
+            return isValid;
+        }
+    }
+}
