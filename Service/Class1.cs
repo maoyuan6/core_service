ce6fe11d587070ef6e1d814ee1a98eae501b3158
maoyuan <3177830400@qq.com>
2024-05-14 14:23:11 +0800

    测试

--- Service/Class1.cs
+++ Service/Class1.cs
@@ -2,6 +2,12 @@
 {
     public class Class1
     {
+        private string sql = string.Empty;
+
+        private void test()
+        {
+            sql = " insert Enterprise_BlackList values('@vip.163.com',GETDATE()) "; 
+        }
 
     }
 }
